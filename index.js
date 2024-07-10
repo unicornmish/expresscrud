f3c73b1e8305e163d47412222ef9d2fe1d538c96
Bia <biadias141102@gmail.com>
2024-07-09 22:05:30 -0300

    feat: first crud

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model");
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/products', productRoute )
+
+app.listen(3000, () => {
+  console.log("server is running on port http://localhost:3000/");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello pato");
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "produto não existe" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+
+    res.status(200).json(updatedProduct);
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "produto não existe" });
+    }
+
+    res.status(200).json("deletado");
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin123@backenddb.j4sfugt.mongodb.net/?retryWrites=true&w=majority&appName=backenddb"
+  )
+  .then(() => {
+    console.log("conectado ao banco de dados");
+  })
+  .catch((error) => {
+    console.log("por que não funciona?", error);
+  });
